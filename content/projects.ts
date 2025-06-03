3d4398c6b1446cfad2c59d871e0c50b4c652380f
rohit133 <rohityou000@gmail.com>
2025-06-04 00:45:02 +0530

    projects

--- content/projects.ts
+++ content/projects.ts
@@ -1,33 +1,44 @@
 interface Project {
-  title: string
-  description: string
-  tags: string[]
-  liveLink: string
-  devLink: string
+  title: string;
+  description: string;
+  tags: string[];
+  liveLink: string;
+  devLink: string;
 }
 
 const projects: Project[] = [
   {
-    title: "Crublibrary",
-    description: "A node package manager with dashboard application to track the usage of api.",
-    tags: ["Next.js", "MongoDB  ", "Express.js"],
-    liveLink: "https://crublibrary-dashboard-crud.vercel.app/",
-    devLink: "https://github.com/rohit133/crublibrary-dashboard",
+    title: "url-shortner api",
+    description:
+      "A scalable, production-ready URL shortener inspired by Bit.ly. This project is designed for high throughput, analytics, and reliability.",
+    tags: ["Express.js", "Node.js", "MongoDB", "Redis"],
+    liveLink: "",
+    devLink: "https://github.com/rohit133/url-shortner",
   },
   {
     title: "Taskmaster api",
-    description: "A backend only infrastructure for creating, assigning and updating task.",
+    description:
+      "A backend only infrastructure for creating, assigning and updating task.",
     tags: ["Express.js", "Node.js", "MongoDB"],
     liveLink: "",
     devLink: "https://github.com/rohit133/taskmaster-api",
   },
   {
+    title: "Crublibrary",
+    description:
+      "A node package manager with dashboard application to track the usage of api.",
+    tags: ["Next.js", "MongoDB  ", "Express.js"],
+    liveLink: "https://crublibrary-dashboard-crud.vercel.app/",
+    devLink: "https://github.com/rohit133/crublibrary-dashboard",
+  },
+  {
     title: "Open D",
-    description: "A Web3 platform using Internet computer blockchain to mint and sell user NTF's.",
+    description:
+      "A Web3 platform using Internet computer blockchain to mint and sell user NTF's.",
     tags: ["React", "JavaScript", "Motoko", "Web3"],
     liveLink: "",
-    devLink: "https://github.com/rohit133/opneD"
-  }
-]
+    devLink: "https://github.com/rohit133/opneD",
+  },
+];
 
 export default projects;
